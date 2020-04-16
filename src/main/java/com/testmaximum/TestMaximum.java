e34e1d6c1ec21737748aca03ed01dbabb15fdecc
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-16 23:47:17 +0530

    ADDED:Given Three Float Number Find Maximum Number

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,10 +1,27 @@
 package com.testmaximum;
 public class TestMaximum
 {
-    //Method to Find Maximum Number
+    //Method to Find Maximum Integer Number
     public static int findMaximumInteger(int firstNumber,int secondNumber,int thirdNumber)
     {
         int maximumNumber=0;
+        if(firstNumber>secondNumber && firstNumber>thirdNumber)
+        {
+            return maximumNumber=firstNumber;
+        }
+        else if (secondNumber>firstNumber && secondNumber>thirdNumber)
+        {
+            return maximumNumber=secondNumber;
+        }
+        else
+        {
+            return maximumNumber=thirdNumber;
+        }
+    }
+    //Method to Find Maximum Number
+    public static double findMaximumFloat( double firstNumber,double secondNumber,double thirdNumber)
+    {
+        double maximumNumber=0;
         if(firstNumber>secondNumber && firstNumber>thirdNumber)
         {
             return maximumNumber=firstNumber;
--- src/test/java/com/testmaximum/FindMaximumTest.java
+++ src/test/java/com/testmaximum/FindMaximumTest.java
@@ -4,22 +4,43 @@ import org.junit.Test;
 public class FindMaximumTest
 {
     TestMaximum findMaximum=new TestMaximum();
+    //TC:1.1
     @Test
     public void givenMaximumNumberAtFirstPosition_shouldReturnFirstNumber()
     {
         Comparable maximumNumber=findMaximum.findMaximumInteger(30,10,20);
         Assert.assertEquals(30,maximumNumber);
     }
+    //TC:1.2
     @Test
-    public void givenMaxIntegerAtSecondPosition_shouldReturnSecondPosition()
+    public void givenMaximumIntegerAtSecondPosition_shouldReturnSecondPosition()
     {
         Comparable maximumNumber=findMaximum.findMaximumInteger(10,30,20);
         Assert.assertEquals(30,maximumNumber);
     }
+    //TC:1.3
     @Test
-    public void givenMaxIntegerAtThirdPosition_shouldReturnThirdPosition()
+    public void givenMaximumIntegerAtThirdPosition_shouldReturnThirdPosition()
     {
         Comparable maximumNumber = findMaximum.findMaximumInteger(10, 20, 30);
         Assert.assertEquals(30, maximumNumber);
+    }
+    //TC:2.1
+    @Test
+    public void givenMaxFloatAtFirstPosition_shouldReturnFirstPosition() {
+        Comparable maximumFloat = findMaximum.findMaximumFloat(30.5,20.5,10.5);
+        Assert.assertEquals(30.5, maximumFloat);
+    }
+    //TC:2.2
+    @Test
+    public void givenMaximumFloatAtSecondPosition_shouldReturnFirstPosition() {
+        Comparable maxFloat = findMaximum.findMaximumFloat(10.5,30.5,20.5);
+        Assert.assertEquals(30.5, maxFloat);
+    }
+    //TC:2.3
+    @Test
+    public void givenMaxFloatAtThirdPosition_shouldReturnFirstPosition() {
+        Comparable maxFloat = findMaximum.findMaximumFloat(10.5, 20.5, 30.5);
+        Assert.assertEquals(30.5, maxFloat);
     }
 }
