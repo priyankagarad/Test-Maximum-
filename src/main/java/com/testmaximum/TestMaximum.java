4e22c9883de9ba29b4ed58f41b07cfd851e9d2f4
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-16 19:02:14 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -0,0 +1,8 @@
+package com.testmaximum;
+public class TestMaximum {
+    public static void main(String args[])
+    {
+        //Welcome message display
+        System.out.println("Welcome to Test Maximum");
+    }
+}
